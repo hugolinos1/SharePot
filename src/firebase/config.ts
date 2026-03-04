c895f9a5dba724a5a2299f8a02ddb9cb279a0928
Hugues R <hugues.rabier@gmail.com>
2026-03-04 17:44:37 +0000

    J'ai aussi une alerte Github : Action needed: Secrets detected in recen

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "dpense-partage",
-  "appId": "1:245126679245:web:d2221fdb98dc21bf32f784",
-  "apiKey": "AIzaSyCzS4G2ijtK9cR1X6WTcnxuBvIEwzAN0m4",
-  "authDomain": "dpense-partage.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "245126679245"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
