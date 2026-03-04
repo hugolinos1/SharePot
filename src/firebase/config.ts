d214c61fd76ffa3051c79afbe56ef55ab95fe53b
Hugues R <hugues.rabier@gmail.com>
2026-03-04 10:37:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "dpense-partage",
+  "appId": "1:245126679245:web:d2221fdb98dc21bf32f784",
+  "apiKey": "AIzaSyCzS4G2ijtK9cR1X6WTcnxuBvIEwzAN0m4",
+  "authDomain": "dpense-partage.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "245126679245"
+};
